61eb1308683de8f6e98c17642cd4928b06093a94
Jim Carroll <thecarrolls@jiminger.com>
2011-05-16 07:07:52 -0400

    Few minor org changes.

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ SWIGROOT=/home/jim/src/swig
 #$SWIGROOT/swig -v -debug-classes -debug-module 1 -debug-symtabs -debug-symbols -debug-csymbols -debug-lsymbols -debug-tags -debug-template -debug-top 1 -debug-typedef -debug-typemap -debug-tmsearch -debug-tmused -c++ -outdir ./ -o ./test_wrap.cpp -plugin ./libipcgen.so -pluginlang ipcgen -I$SWIGROOT/Lib/python -I./Lib/ipcgen -I$SWIGROOT/Lib test.i
 
 $SWIGROOT/swig -xmlout ./test.xml -v -c++ -outdir ./ -o ./test_parent_wrap.cpp -plugin ./libipcgenp.so -pluginlang ipcgen -I./Lib/ipcgen -I$SWIGROOT/Lib test.i
-$SWIGROOT/swig -xmlout ./test.xml -v -c++ -outdir ./ -o ./test_child_wrap.cpp -plugin ./libipcgenc.so -pluginlang ipcgen -I./Lib/ipcgen -I$SWIGROOT/Lib test.i
+#$SWIGROOT/swig -xmlout ./test.xml -v -c++ -outdir ./ -o ./test_child_wrap.cpp -plugin ./libipcgenc.so -pluginlang ipcgen -I./Lib/ipcgen -I$SWIGROOT/Lib test.i
 
 #$SWIGROOT/swig -xmlout ./test.xml -v -c++ -outdir ./ -o ./test_wrap.cpp -plugin ./libipcgen.so -pluginlang ipcgen -I./Lib/ipcgen -I$SWIGROOT/Lib  ~/src/xbmc/xbmc/interfaces/python/module/swig/AddonModuleXbmcgui.i
 
