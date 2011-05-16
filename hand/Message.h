0066d34bfe90c9d07ec94670e7fb2f94e1fb104e
Jim Carroll <thecarrolls@jiminger.com>
2011-05-16 10:48:52 -0400

    Added docs to the Message class.

--- hand/Message.h
+++ hand/Message.h
@@ -14,6 +14,43 @@ namespace XbmcIpc
     MessageException(const char* message_) : message(message_) {}
   };
 
+  /**
+   * This class is based on the java java.nio.Buffer class however, it 
+   *  does not implement the 'mark' functionality. It's meant to
+   *  hold a message for sending over the MessageChannel.
+   *
+   * [ the following is borrowed from the javadocs for java.nio.Buffer 
+   * where it applies to this class]:
+   *
+   * A buffer is a linear, finite sequence of elements of a unspecified types.
+   * Aside from its content, the essential properties of a buffer are its capacity, 
+   *   limit, and position:
+   *    A buffer's capacity is the number of elements it contains. The capacity 
+   *      of a buffer is never negative and never changes.
+   *
+   *    A buffer's limit is the index of the first element that should not be 
+   *      read or written. A buffer's limit is never negative and is never greater 
+   *      than its capacity.
+   *
+   *     A buffer's position is the index of the next element to be read or written. 
+   *       A buffer's position is never negative and is never greater than its limit.
+   *
+   * Invariants:
+   *
+   * The following invariant holds for the mark, position, limit, and capacity values:
+   *
+   *     0 <= mark <= position <= limit <= capacity 
+   *
+   * A newly-created buffer always has a position of zero and a limit set to the 
+   * capacity. The initial content of a buffer is, in general, undefined. 
+   *
+   * Example:
+   *  Message message(1024);
+   *  message.putInt(1).putString("hello there").putLongLong( ((long long)2)^40 );
+   *  message.flip();
+   *  printf("message contents: %d, \"%s\", %ll\n", message.getInt(), 
+   *         message.getCharPointerDirect(), message.getLongLong());
+   */
   class Message
   {
     typedef unsigned char byte;
@@ -31,12 +68,59 @@ namespace XbmcIpc
     }
 
   public:
+    /**
+     * Construct a message given an externally managed memory buffer.
+     * The ownership of the buffer is assumed to be the code that called
+     * this constructor, therefore the Message descrutor will not free it.
+     */
     Message(void* buffer_, size_t bufferSize) : buffer((byte*)buffer_), mcapacity(bufferSize), owner(false) { clear(); }
+
+    /**
+     * Construct a message buffer using the size buffer provided. The
+     * buffer will be internally managed and will be freed upon 
+     * destruction.
+     */
     Message(size_t bufferSize) : buffer(new byte[bufferSize]), mcapacity(bufferSize), owner(true) { clear(); }
     ~Message() { if (owner) delete [] buffer; }
 
-    Message& flip() { mlimit = mposition; mposition = 0; return *this; }
+    /**
+     * Flips this buffer. The limit is set to the current position 
+     *   and then the position is set to zero. 
+     *
+     * After a sequence of channel-read or put operations, invoke this 
+     *   method to prepare for a sequence of channel-write or relative 
+     *   get operations. For example:
+     *
+     * buf.put(magic);    // Prepend header
+     * in.read(buf);      // Read data into rest of buffer
+     * buf.flip();        // Flip buffer
+     * out.write(buf);    // Write header + data to channel
+     *
+     * This is used to prepare the Message for reading from after
+     *  it has been written to.
+     */
+     Message& flip() { mlimit = mposition; mposition = 0; return *this; }
+
+    /**
+     *Clears this buffer. The position is set to zero, the limit 
+     *  is set to the capacity.
+     *
+     * Invoke this method before using a sequence of channel-read 
+     *  or put operations to fill this buffer. For example:
+     *
+     *     buf.clear();     // Prepare buffer for reading
+     *     in.read(buf);    // Read data
+     *
+     * This method does not actually erase the data in the buffer, 
+     *  but it is named as if it did because it will most often be used 
+     *  in situations in which that might as well be the case. 
+     */
     Message& clear() { mlimit = mcapacity; mposition = 0; return *this; }
+
+    /**
+     * This method resets the position to the beginning of the buffer
+     *  so that it can be either reread or written to all over again.
+     */
     Message& rewind() { mposition = 0; return *this; }
 
     Message& put(const void* src, size_t bytes) throw(MessageException) { check(bytes); memcpy( buffer + mposition, src, bytes); mposition += bytes; return *this; }
